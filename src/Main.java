cfed7d22cf4b4078924229c9b2494d8373e1e015
Manikandan M P <manikandanmperikamana@gmail.com>
2026-01-07 17:48:36 +0530

    Add a new donor
    Can add new donor with details and it will be stored in the manager file.

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,49 @@ public class Main{
         // System.out.println("Availability:"+d1.isAvailable());
 
         DonorManager manager=new DonorManager();
-
-        Donor d1=new Donor("Ravi","O+",19,true);
-        Donor d2=new Donor("Kavi","A+",19,false);
-        manager.addDonor(d1);
-        manager.addDonor(d2);
-
-        // manager.showAllDonors();
-
+        
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the Blood Group to serach:");
-        String bg=sc.nextLine();
-        manager.searchByBloodGroup(bg);
+        int choice;
+        while(true)
+        {
+            System.out.println("Donor Management System:\n1.Add Donor\n2.Show All Donors\n3.Search By Blood Group\n4.Exit\nEnter Your choice:");
+            choice=sc.nextInt();
+            sc.nextLine();
+            switch(choice)
+            {
+                case 1:
+                    System.out.println("Enter the name:");
+                    String name=sc.nextLine();
+                    System.out.println("Enter Age:");
+                    int age=sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Enter Blood Group:");
+                    String bloodgroup=sc.nextLine();
+                    System.out.println("Available?:");
+                    boolean available=sc.nextBoolean();
+                    sc.nextLine();
+                    Donor newDonor=new Donor(name,bloodgroup,age,available);
+                    manager.addDonor(newDonor);
+                    System.out.println("Donor added successfully!");
+    
+
+                    break;
+
+                case 2:
+                    manager.showAllDonors();
+                    break;
+                case 3:
+                    System.out.println("Enter the Blood Group:");
+                    String bg=sc.nextLine();
+                    manager.searchByBloodGroup(bg);
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("Invalid choice!!");
+
+            }
+        }
+
      }
 }
