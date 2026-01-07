9e0dba8a34b97b3a12dd84d1994e602b4da8f14c
Manikandan M P <manikandanmperikamana@gmail.com>
2026-01-07 12:05:05 +0530

    Blood group
    Created bloogroup field to store blood gorups of the person.

--- src/Donor.java
+++ src/Donor.java
@@ -1,11 +1,17 @@
 public class Donor {
     private String name;
-    public Donor(String name)
+    private String bloodgroup;
+    public Donor(String name,String bloodgroup)
     {
         this.name=name;
+        this.bloodgroup=bloodgroup;
     }
     public String getname()
     {
         return name;
+    }
+    public String getbg()
+    {
+        return bloodgroup;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] args) {
-        Donor d1=new Donor("Mani");
+        Donor d1=new Donor("Mani","o+");
         System.out.println("Donor Name: " + d1.getname());
-        
+        System.out.println("Blood Group:"+d1.getbg());
     }
 }
